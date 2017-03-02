bdd5e6088fba7149a3a2a355ed55fe27e5e41928
babestvl <babestvl@gmail.com>
2017-03-02 11:35:14 +0700

    update code design

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,23 @@
 public class Game {
-    public boolean check(Board board, int putX, int putY) {
+    private Player[] p;
+    private Board board;
+
+    public Game(int row, int column, String name1, String name2) {
+        board = new Board(row, column);
+        p = new Player[2];
+        p[0] = new Player(name1, "X");
+        p[1] = new Player(name2, "O");
+    }
+
+    public boolean check(int putX, int putY) {
         String s = "";
+        board.print();
         for (int i = -5; i < 5; i++) {
             if (putY + i >= 0 && putY + i < 9) {
                 s += board.getSquares()[putX][putY + i];
             }
         }
-        if(s.contains("XXXXX")||s.contains("OOOOO")){
+        if (s.contains("XXXXX") || s.contains("OOOOO")) {
             return true;
         }
 
@@ -16,7 +27,7 @@ public class Game {
                 s += board.getSquares()[putX + i][putY];
             }
         }
-        if(s.contains("XXXXX")||s.contains("OOOOO")){
+        if (s.contains("XXXXX") || s.contains("OOOOO")) {
             return true;
         }
 
@@ -25,23 +36,30 @@ public class Game {
             if (putX + i >= 0 && putX + i < 9 && putY + i >= 0 && putY + i < 9) {
                 s += board.getSquares()[putX + i][putY + i];
             }
+            if (s.contains("XXXXX") || s.contains("OOOOO")) {
+                return true;
+            }
         }
-        if(s.contains("XXXXX")||s.contains("OOOOO")){
-            return true;
-        }
-
         s = "";
         for (int i = -5; i < 5; i++) {
             if (putX - i >= 0 && putX - i < 9 && putY + i >= 0 && putY + i < 9) {
                 s += board.getSquares()[putX - i][putY + i];
             }
         }
-        if(s.contains("XXXXX")||s.contains("OOOOO")){
+        if (s.contains("XXXXX") || s.contains("OOOOO")) {
             return true;
         }
 
         return false;
     }
 
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player[] getP() {
+        return p;
+    }
 
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    private Scanner scanner = new Scanner(System.in);
-    public void start(Game game) {
-        int row = 9;
-        int column = 9;
-        Board board = new Board(row,column);
-        System.out.print("First player name: ");
-        Player p1 = new Player(scanner.nextLine(), "X");
-        System.out.print("Second player name: ");
-        Player p2 = new Player(scanner.nextLine(), "O");
-        Player[] p = {p1,p2};
 
-        for (int i = 0; true; i++){
-            System.out.println(p[i%2].getName()+"'s turn.");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("First player name: ");
+        String name1 = scanner.nextLine();
+        System.out.print("Second player name: ");
+        String name2 = scanner.nextLine();
+        Game g = new Game(9, 9, name1, name2);
+        for (int i = 0; true; i++) {
+            System.out.println(g.getP()[i%2].getName() + "'s turn.");
             System.out.print("Please select row: ");
             int putX = scanner.nextInt();
             System.out.print("Please select column: ");
             int putY = scanner.nextInt();
-            p[i%2].placeSymbol(board, putX, putY);
-            System.out.printf("You placed %s on (%d,%d)\n",p[i%2].getSymValue(), row, column);
-            board.print();
-            if (game.check(board, putX - 1, putY - 1)) {
-                System.out.printf("%s player win!\n",p[i%2].getName());
+            System.out.printf("You placed %s on (%d,%d)\n", g.getP()[i%2].getSymValue(), putX, putY);
+            g.getP()[i%2].placeSymbol(g.getBoard(), putX, putY);
+            if (g.check(putX - 1, putY - 1)) {
+                System.out.printf("%s player win!\n", g.getP()[i%2].getName());
                 break;
             }
+
         }
-
     }
 
-    public static void main(String[] args) {
-        Main m = new Main();
-        m.start(new Game());
-    }
 }
