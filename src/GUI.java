9378245fe01be4d129d1cd4e8df33a8d0b8498a8
oatThanut <oatThanut@Thanuts-MacBook-Pro.local>
2017-03-07 00:22:35 +0700

    Create GUI class and init

---
+++ src/GUI.java
@@ -0,0 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 300);
+		contentPane = new JPanel();
+		contentPane.setLayout(null);
+		
+
+		setContentPane(contentPane);
+	}
+
+}
